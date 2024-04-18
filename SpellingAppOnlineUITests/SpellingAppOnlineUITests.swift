0b5987fa4d34446f9b96ce2396d55ed85508c8db
asdf <asdf@asdf.com>
2024-04-18 11:22:50 +0100

    Fixing tests run that were timing out

--- SpellingAppOnlineUITests/SpellingAppOnlineUITests.swift
+++ SpellingAppOnlineUITests/SpellingAppOnlineUITests.swift
@@ -21,12 +21,4 @@ final class SpellingAppOnlineUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
