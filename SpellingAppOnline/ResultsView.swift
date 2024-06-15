eda75696bafbed00b969b20c2d7032045e1114a3
getOffIt <nedjyzer@gmail.com>
2024-06-15 10:54:32 +0100

    Making the result view easier to read

--- SpellingAppOnline/ResultsView.swift
+++ SpellingAppOnline/ResultsView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 struct ResultsView: View {
     @State var score: Double = 50 // Replace with actual score
-    @State private var currentTime = Date() // Initializes with the current date and time
+    @State private var currentTime = Date()
     
     @State var completionDate:String = "13 Feb 2024 at 10:34"
     let duration = ""
@@ -11,7 +11,6 @@ struct ResultsView: View {
     @Binding var testStatus: TestStatus
     @Binding var answers: [String]
     var questions: [String]
-    
     
     @State var correct = 0
     @State var pass = true
@@ -27,7 +26,7 @@ struct ResultsView: View {
         questions.indices.filter { answers.indices.contains($0) && answers[$0] != questions[$0] }.map { questions[$0] }
     }
     
-    //Share related
+    // Share related
     @StateObject var imageManager: ImageManager = ImageManager()
     @Environment(\.displayScale) var displayScale
     @State private var imageShareTransferable: ImageManager.ImageShareTransferable?
@@ -40,21 +39,27 @@ struct ResultsView: View {
                         .font(.largeTitle)
                         .fontWeight(.bold)
                     
-                    VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 10) { // Adjusted spacing
                         HStack {
                             VStack(alignment: .leading) {
                                 Text("SCORE")
                                     .fontWeight(.bold)
-                                Text("\(Int(score))%").onAppear(){ score = calculateScore() }.foregroundColor(pass ? .green : .red)
-                                Text("\(correct)/\(questions.count)").foregroundColor(pass ? .green : .red)
+                                Text("\(Int(score))%")
+                                    .foregroundColor(pass ? .green : .red)
+                                    .onAppear {
+                                        score = calculateScore()
+                                    }
+                                Text("\(correct)/\(questions.count)")
+                                    .foregroundColor(pass ? .green : .red)
                             }
                             Spacer()
                             VStack(alignment: .leading) {
                                 Text("COMPLETED")
                                     .fontWeight(.bold)
-                                Text(completionDate).onAppear(){
-                                    completionDate = dateFormatter.string(from: currentTime)
-                                }
+                                Text(completionDate)
+                                    .onAppear {
+                                        completionDate = dateFormatter.string(from: currentTime)
+                                    }
                             }
                             Spacer()
                             VStack(alignment: .leading) {
@@ -70,40 +75,34 @@ struct ResultsView: View {
                         Text("QUESTION")
                         Spacer()
                         Text("RESPONSE").frame(width: 100, alignment: .center)
-                        Spacer()
                     }
+                    .padding(.horizontal)
+                    
                     if answers.count == questions.count {
                         ForEach(Array(zip(questions.indices, questions)), id: \.0) { index, question in
                             VStack(alignment: .leading) {
                                 HStack {
-                                    VStack {
-                                        HStack {
-                                            Text("\(index + 1).")
-                                                .bold()
-                                            Text(question)
-                                        }
-                                    }
+                                    Text("\(index + 1). \(question)")
                                     Spacer()
-                                    
-                                    VStack {
-                                        Text(answers[index]).frame(alignment: .center)
-                                    }
-                                    Spacer()
+                                    Text(answers[index])
+                                        .frame(width: 100, alignment: .center)
                                     Image(systemName: answers[index] == questions[index] ? "checkmark.circle.fill" : "xmark.circle.fill")
                                         .foregroundColor(answers[index] == questions[index] ? .green : .red)
                                 }
-                                
+                                .padding(.horizontal)
+                                .padding(.vertical, 5)
+                                .background(index % 2 == 0 ? Color(UIColor.systemGray6) : Color(UIColor.systemGray5)) // Alternating background colors
+                                .cornerRadius(8) // Rounded corners for background
                             }
-                            .padding(.horizontal)
                         }
                     }
+                    
                     Button(action: {
                         // Play again action
-                        if (incorrectAnswers.isEmpty) {
+                        if incorrectAnswers.isEmpty {
                             testStatus = .spelling
                             answers = []
-                        }
-                        else {
+                        } else {
                             testStatus = .learning
                         }
                     }) {
@@ -112,10 +111,11 @@ struct ResultsView: View {
                             .frame(maxWidth: .infinity)
                             .padding()
                             .background(Color.purple)
-                            .cornerRadius(10)
+                            .cornerRadius(10) // Enhanced button appearance
                     }
                     .padding()
-                    if (RemoteConfigManager.shared.sharingResultsEnabled) {
+                    
+                    if RemoteConfigManager.shared.sharingResultsEnabled {
                         if let imageShareTransferable = imageShareTransferable {
                             ShareLink(
                                 item: imageShareTransferable,
@@ -125,34 +125,31 @@ struct ResultsView: View {
                                         HStack {
                                             Image(systemName: "square.and.arrow.up")
                                             Text("Share my Results")
-                                            
                                         }
                                     }
                                     .padding()
                         }
                     }
                 }
+                .padding()
                 .frame(maxWidth: .infinity)
             }
             .onAppear {
-                
                 if incorrectAnswers.isEmpty {
                     continueText = "Play again"
                 } else {
                     continueText = "Continue to Learn words page"
                 }
-                if (RemoteConfigManager.shared.sharingResultsEnabled) {
-                    imageShareTransferable = imageManager.getTransferable(ResultsViewShare(score: calculateScore(), pass: pass, correctAnswers: correct, questions: questions,answers: answers , completionDate: completionDate), scale: displayScale, caption: "My Results")
+                if RemoteConfigManager.shared.sharingResultsEnabled {
+                    imageShareTransferable = imageManager.getTransferable(ResultsViewShare(score: calculateScore(), pass: pass, correctAnswers: correct, questions: questions, answers: answers, completionDate: completionDate), scale: displayScale, caption: "My Results")
                 }
             }
-            .padding()
-            .frame(maxHeight: .infinity)
         }
     }
     
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd MMM yyyy 'at' HH:mm" // Set date format as per your requirement
+        formatter.dateFormat = "dd MMM yyyy 'at' HH:mm"
         return formatter
     }
     
@@ -165,21 +162,14 @@ struct ResultsView: View {
                     correct += 1
                 }
             }
-        }
-        else {
+        } else {
             correct = 1 // I've only had this happen when running on the simulator and using the enter key on keyboard when during the spelling test phase
         }
         let percentage = Double(correct) / Double(total) * 100
-        if percentage > 89.40 {
-            pass = true
-        }
-        else {
-            pass = false
-        }
+        pass = percentage > 89.4
         return percentage
     }
 }
-
 
 struct ResultsView_Previews: PreviewProvider {
     static var previews: some View {
