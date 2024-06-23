ed288215b9a3460158dd5061c774c6ebe0e9a6a2
getOffIt <nedjyzer@gmail.com>
2024-06-23 21:37:09 +0100

    removed dead code

--- SpellingAppOnline/ContentView.swift
+++ SpellingAppOnline/ContentView.swift
@@ -12,30 +12,11 @@ struct ContentView: View {
     @ObservedObject var remoteConfigLocal = RemoteConfigManager.shared
     
     // Main test for Chloe
-    @State private var testCompletedFull = false
-    @State private var testNeedsLearning = false
     @State private var testStatusFull = TestStatus.spelling
     @State private var answersFull: [String] = []
         private var questionsFull: [String] {get {RemoteConfigManager.shared.firsttabItemWordList}}
 //    private var questionsFull = WordsData().testDataHomophones
-    // Small tests for words not mastered Legacy
-    @State private var testCompletedSmall = false
-    @State private var testStatusSmall = TestStatus.learning
-    @State private var answersSmall: [String] = []
-    private var questionsSmall: [String] {get {RemoteConfigManager.shared.secondTabItemWordList}}
-    
-    
-    // French test Chloe
-    @State private var testCompletedFrench = false
-    @State private var testStatusFrench = TestStatus.learning
-    @State private var questionsFrench: [String] = WordsData().wordsFrench
-    @State private var answersFrench: [String] = []
-    
-    // Leo's test
-    @State private var testCompletedLeoFull = false
-    @State private var testStatusLeo = TestStatus.learning
-    @State private var questionsLeoFull: [String] = WordsData().yearOnewords10
-    @State private var answersLeoFull: [String] = []
+        
     
     private var enableTabBarForDebug = false
     @State private var testStatusResultsDebug = TestStatus.reviewing
