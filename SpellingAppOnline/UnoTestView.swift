4cac1bf73eb02fa991da470c847cd260112d6767
getOffIt <nedjyzer@gmail.com>
2024-07-08 17:55:27 +0100

    Fixed issue with Uno view by simplifying logic

--- SpellingAppOnline/UnoTestView.swift
+++ SpellingAppOnline/UnoTestView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct UnoTestView: View {
     @State private var isPresentingDetailView = false
-    
     @State private var selectedWordCount = 1
-    @State private var spellingTestMetadata: SpellingTestMetadata
-    @State private var temporaryunoTestMetadata: SpellingTestMetadata
+    private var spellingTestMetadata: SpellingTestMetadata
+    
     init(spellingTestMetadata: SpellingTestMetadata) {
         self.spellingTestMetadata = spellingTestMetadata
-        self.temporaryunoTestMetadata = SpellingTestMetadata(questions: spellingTestMetadata.questions)
     }
     
     var body: some View {
@@ -31,12 +29,8 @@ struct UnoTestView: View {
             .onAppear {
                 selectedWordCount = min(spellingTestMetadata.questions.count, 1)
             }
-            .onSubmit {
-
-            }
+            
             Button(action: {
-                let reducedQuestions: [String] = reduceArray(spellingTestMetadata.questions, to: selectedWordCount)
-                temporaryunoTestMetadata = SpellingTestMetadata(questions: reducedQuestions)
                 isPresentingDetailView = true
             }) {
                 Text("Start Uno test")
@@ -47,28 +41,21 @@ struct UnoTestView: View {
                     .cornerRadius(10)
             }
             .sheet(isPresented: $isPresentingDetailView) {
-                DetailView(spellingTestMetadata: temporaryunoTestMetadata, isPresented: $isPresentingDetailView)
+                DetailView(questions: Array(spellingTestMetadata.questions.prefix(selectedWordCount)), isPresented: $isPresentingDetailView)
             }
         }
         
-    }
-    
-    func reduceArray<T>(_ array: [T], to count: Int) -> [T] {
-        return Array(array.prefix(count))
     }
 }
 
 
 struct DetailView: View {
+    var questions: [String]
     @Binding var isPresented: Bool
-    @State private var spellingTestMetadata: SpellingTestMetadata
-    
-    init(spellingTestMetadata: SpellingTestMetadata, isPresented: Binding<Bool>) {
-        self.spellingTestMetadata = spellingTestMetadata
-        _isPresented = isPresented
-    }
     
     var body: some View {
+        let spellingTestMetadata = SpellingTestMetadata(questions: questions)
+        
         VStack {
             // Dismiss button handled in the presenting view
             Button(action: {
@@ -76,9 +63,9 @@ struct DetailView: View {
             }) {
                 Spacer()
                 Image(systemName: "xmark.circle.fill")
-                                         .font(.title)
-                                         .foregroundColor(.gray)
-                                         .padding()
+                    .font(.title)
+                    .foregroundColor(.gray)
+                    .padding()
             }
         }
         FullTestSequence(spellingTestMetadata: spellingTestMetadata)
