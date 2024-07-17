e0b41b66ae97d07726d9c20b2f594f989238a071
getOffIt <nedjyzer@gmail.com>
2024-07-17 19:14:41 +0100

    alright

--- SpellingAppOnline/YearViewProgress.swift
+++ SpellingAppOnline/YearViewProgress.swift
@@ -16,35 +16,53 @@ struct YearViewProgress: View {
     
     var body: some View {
         VStack {
-            Text("Year 6 progress is \(progress)%").onAppear(){ progress = CalculateProgress().progressPercentage()}
-            Text("You know \(current) words out of \(total)").onAppear(){ current = CalculateProgress().current(); total = CalculateProgress().total()}
+            HStack {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.green)
+                Text("Year 6: \(CalculateProgress().progressPercentage())%")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("Completed")
+                    .foregroundColor(.gray)
+            }
+            .padding(.bottom, 10)
+            
+            ProgressView(value: 4, total: 8)
+                .progressViewStyle(LinearProgressViewStyle(tint: .green))
+                .scaleEffect(x: 1, y: 4, anchor: .center)
+                .padding([.leading, .trailing], 20)
         }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
     }
 }
-
-struct CalculateProgress {
-    let currentProgress = WordsData().allWordsYear6Part1.count + WordsData().allWordsYear6Part2.count + WordsData().allWordsYear6Part3.count
-   
-    func calculate() -> Int {
-        return 55
+    
+    
+    struct CalculateProgress {
+        let currentProgress = WordsData().allWordsYear6Part1.count + WordsData().allWordsYear6Part2.count + WordsData().allWordsYear6Part3.count
+        
+        func calculate() -> Int {
+            return 55
+        }
+        
+        func current() -> Int {
+            return currentProgress
+        }
+        
+        func progressPercentage() -> Int {
+            let totalWords = total()
+            guard totalWords > 0 else { return 0 }
+            let progress = current()
+            return (progress * 100) / totalWords
+        }
+        
+        func total() -> Int {
+            return WordsData().allWordsYear6.count
+        }
+        
     }
     
-    func current() -> Int {
-        return currentProgress
-    }
-    
-    func progressPercentage() -> Int {
-           let totalWords = total()
-           guard totalWords > 0 else { return 0 }
-           let progress = current()
-           return (progress * 100) / totalWords
-       }
-    
-    func total() -> Int {
-        return WordsData().allWordsYear6.count
-    }
-    
-}
-
-// currently in part 4
-// all words
+    // currently in part 4
+    // all words
