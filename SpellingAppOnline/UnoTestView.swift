4889fcd956f660fbaa85822272a4d630046d530f
getOffIt <nedjyzer@gmail.com>
2024-08-15 17:17:55 +0200

    same for this view

--- SpellingAppOnline/UnoTestView.swift
+++ SpellingAppOnline/UnoTestView.swift
@@ -17,33 +17,37 @@ struct UnoTestView: View {
     }
     
     var body: some View {
-        
-        VStack {
-            Text("Uno Game")
-                .font(.largeTitle)
-            Picker("Word Count", selection: $selectedWordCount) {
-                ForEach(1...spellingTestMetadata.questions.count, id: \.self) { count in
-                    Text("\(count) words")
+        ZStack {
+            VStack {
+                Text("Uno Game")
+                    .font(.largeTitle)
+                Picker("Word Count", selection: $selectedWordCount) {
+                    ForEach(1...spellingTestMetadata.questions.count, id: \.self) { count in
+                        Text("\(count) words")
+                    }
+                }
+                .onAppear {
+                    selectedWordCount = min(spellingTestMetadata.questions.count, 1)
+                }
+                
+                Button(action: {
+                    isPresentingDetailView = true
+                }) {
+                    Text("Start Uno test")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .sheet(isPresented: $isPresentingDetailView) {
+                    DetailView(questions: Array(spellingTestMetadata.questions.prefix(selectedWordCount)), isPresented: $isPresentingDetailView)
                 }
             }
-            .onAppear {
-                selectedWordCount = min(spellingTestMetadata.questions.count, 1)
-            }
-            
-            Button(action: {
-                isPresentingDetailView = true
-            }) {
-                Text("Start Uno test")
-                    .font(.headline)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .sheet(isPresented: $isPresentingDetailView) {
-                DetailView(questions: Array(spellingTestMetadata.questions.prefix(selectedWordCount)), isPresented: $isPresentingDetailView)
-            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .foregroundStyle(.white)
+        .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.blue]), startPoint: .top, endPoint: .bottom))
         
     }
 }
