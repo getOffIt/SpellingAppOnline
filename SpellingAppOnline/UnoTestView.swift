5a414e394a37f3a604988059115bcce61eeefe09
getOffIt <nedjyzer@gmail.com>
2024-08-16 10:10:39 +0200

    tyding up the Uno game view too

--- SpellingAppOnline/UnoTestView.swift
+++ SpellingAppOnline/UnoTestView.swift
@@ -18,34 +18,52 @@ struct UnoTestView: View {
     
     var body: some View {
         ZStack {
-            VStack {
+            VStack(spacing: 20) {
                 Text("Uno Game")
                     .font(.largeTitle)
-                Picker("Word Count", selection: $selectedWordCount) {
-                    ForEach(1...spellingTestMetadata.questions.count, id: \.self) { count in
-                        Text("\(count) words")
+                    Spacer()
+                if spellingTestMetadata.questions.count < 10 {
+                    Section("How many words do you have to do?") {
+                        Picker("Word Count", selection: $selectedWordCount) {
+                            ForEach(1...spellingTestMetadata.questions.count, id: \.self) { count in
+                                Text("\(count)")
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .onAppear {
+                            selectedWordCount = min(spellingTestMetadata.questions.count, 1)
+                        }
+                    }
+                } else {
+                    Picker("Word Count", selection: $selectedWordCount) {
+                        ForEach(1...spellingTestMetadata.questions.count, id: \.self) { count in
+                            Text("\(count) words")
+                        }
+                    }
+                    .onAppear {
+                        selectedWordCount = min(spellingTestMetadata.questions.count, 1)
                     }
                 }
-                .onAppear {
-                    selectedWordCount = min(spellingTestMetadata.questions.count, 1)
-                }
-                
                 Button(action: {
                     isPresentingDetailView = true
                 }) {
                     Text("Start Uno test")
+                        .frame(maxWidth: .infinity)
                         .font(.headline)
                         .padding()
-                        .background(Color.blue)
+                        .background(Color.secondary)
                         .foregroundColor(.white)
                         .cornerRadius(10)
+                        
                 }
                 .sheet(isPresented: $isPresentingDetailView) {
                     DetailView(questions: Array(spellingTestMetadata.questions.prefix(selectedWordCount)), isPresented: $isPresentingDetailView)
                 }
+                Spacer()
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
         .foregroundStyle(.white)
         .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.blue]), startPoint: .top, endPoint: .bottom))
         
